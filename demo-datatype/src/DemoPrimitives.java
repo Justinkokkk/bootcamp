ca03a92046847ee31a2af5fed8b44bc8a265daf4
justinko <s40558430123@gmail.com>
2024-05-08 21:54:21 +0800

    update

---
+++ demo-datatype/src/DemoPrimitives.java
@@ -0,0 +1,13 @@
+public class DemoPrimitives {
+  public static void main(String[] args) {
+    int x = 3;
+    int y = 3;
+    int a = 4;
+    int b = 4;
+    int total = (x+ y-a)*b/a%1;
+    
+    
+    System.out.println(total);
+    
+  }
+}
---
+++ demo-datatype/src/demostring.java
@@ -0,0 +1,9 @@
+public class demostring {
+  public static void main (String[] args) {
+    String x=">";
+    String y="<";
+    String z=x+y;
+    System.out.println(z);
+  }
+  
+}
